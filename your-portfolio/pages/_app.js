5480c0c79c09cdde91793bbcbc2e7e7976e477f0
Folasade Agbaje <folasadeagbaje@gmail.com>
2021-08-22 17:13:00 +0100

    chore: setup theme object

--- your-portfolio/pages/_app.js
+++ your-portfolio/pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// 1. Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  red: {
+    400: '#F56565',
+    600: '#C53030',
+  },
+  gray: {
+    200: '#E2E8F0',
+    400: '#A0AEC0',
+  },
+};
+
+const theme = extendTheme({ colors });
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
